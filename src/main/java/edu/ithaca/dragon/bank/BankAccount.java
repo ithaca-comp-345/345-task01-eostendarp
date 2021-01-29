6b009ea9e236bc6dfcbd1d75c57ee523205d4d49
eostendarp <eostendarp@ithaca.edu>
2021-01-28 23:32:38 -0500

    update withdraw implementation

--- src/main/java/edu/ithaca/dragon/bank/BankAccount.java
+++ src/main/java/edu/ithaca/dragon/bank/BankAccount.java
@@ -27,13 +27,16 @@ public class BankAccount {
     }
 
     /**
-     * @post reduces the balance by amount if amount is non-negative and smaller than balance
+     * Subracts amount from balance if amount is non-negative and greater than or equal to balance
+     * @param amount value to withdraw from balance
+     * @throws InsufficientFundsException
      */
     public void withdraw (double amount) throws InsufficientFundsException{
-        if (amount <= balance){
+        if (amount < 0) {
+            throw new IllegalArgumentException("amount must be non-negative");
+        } else if (amount <= balance) {
             balance -= amount;
-        }
-        else {
+        } else {
             throw new InsufficientFundsException("Not enough money");
         }
     }
